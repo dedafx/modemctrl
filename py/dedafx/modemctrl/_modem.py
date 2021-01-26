d338054a29eecda74c0ff16e0eeccf4770257ef4
Ben Deda <ben@dedafx.com>
2021-01-25 21:51:51 -0800

    use gloal pin for modem control

--- py/dedafx/modemctrl/_modem.py
+++ py/dedafx/modemctrl/_modem.py
@@ -5,32 +5,31 @@ import gpiozero
 RELAY_PIN = 23
 INVERT_PIN = True
 TIME_OFF = 30.0
+pin = gpiozero.LED(pin)
 
 
-def turnModemOn(pin=RELAY_PIN, invertPin=INVERT_PIN):
+def turnModemOn(invertPin=INVERT_PIN):
     """Turn the modem on."""
-    pin = gpiozero.LED(pin)
     if not invertPin:
         pin.on()
     else:
         pin.off()
 
 
-def turnModemOff(pin=RELAY_PIN, invertPin=INVERT_PIN):
+def turnModemOff(invertPin=INVERT_PIN):
     """Turn the modem off."""
-    pin = gpiozero.LED(pin)
     if not invertPin:
         pin.off()
     else:
         pin.on()
 
 
-def reboot(relayPin=RELAY_PIN, timeOff=TIME_OFF, invertPin=INVERT_PIN):
+def reboot(timeOff=TIME_OFF, invertPin=INVERT_PIN):
     """
     Reboot the modem using the given pin.
     Leave the modem off for the given ammount of time.
     invertPin is the off value is high, vs off is low (default).
     """
-    turnModemOff(pin=relayPin, invertPin=invertPin)
+    turnModemOff(invertPin=invertPin)
     time.sleep(timeOff)
-    turnModemOn(pin=relayPin, invertPin=invertPin)
+    turnModemOn(invertPin=invertPin)
